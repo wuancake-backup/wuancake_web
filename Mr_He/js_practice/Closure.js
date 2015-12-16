0e157592f6fdc3550c689808bec72c038d3fe578
Xin <396460741@qq.com>
2015-12-16 09:02:16 +0800

    练习修改

--- Mr_He/js_practice/Closure.js
+++ Mr_He/js_practice/Closure.js
@@ -26,3 +26,47 @@ function createFunction2(){
 var result2 = createFunction2();
 result2[0]();//0
 result2[1]();//1
+
+function createFunction3(){
+    var result = new Array();
+    for(var i = 0; i<10;i++){
+        result[i] = (function(){
+            var num = i;
+            return function(){
+                return num;
+            };
+        })();
+    }
+    return result;
+}
+var result3 = createFunction3();
+result3[0]();//0
+result3[1]();//1
+
+function createFunction4(){
+    var result = new Array();
+    for(var i = 0; i<10;i++){
+        result[i] = function(){
+            var num = i;
+            return function(){
+                return num;
+            };
+        };
+    }
+    return result;
+}
+var result4 = createFunction4();
+result4[0]()();//10
+
+var name = "The Window";
+var object = {
+    name:"My Object",
+    getNameFunc:function(){
+        return function (){
+            return this.name;
+        };
+    }
+};
+object.getNameFunc()();
+
+
