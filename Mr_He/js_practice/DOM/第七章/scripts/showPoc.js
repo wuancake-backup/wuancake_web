4202479dfde1d37d2ddc22d3ce5c40835effe942
Xin <396460741@qq.com>
2015-12-21 08:12:19 +0800

    DOM练习

---
+++ Mr_He/js_practice/DOM/第七章/scripts/showPoc.js
@@ -0,0 +1,87 @@
+/* 
+* @Author: anchen
+* @Date:   2015-12-15 14:24:47
+* @Last Modified by:   anchen
+* @Last Modified time: 2015-12-18 16:52:51
+*/
+
+'use strict';
+function showPic(whichpic){
+    if(!document.getElementById("placeholder")) return false;//如果图片占位符不存在，图片切换不成功，返回false
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    placeholder.setAttribute("src",source);
+
+    if(document.getElementById("description")){
+    var text = whichpic.getAttribute("title");
+    var description = document.getElementById("description");
+    description.firstChild.nodeValue = text;
+    }//如果描述文字存在，则进行描述文字更新，不存在就忽略，不会对图片更新产生影响
+
+    return true;//图片更新成功
+}//图片和文字更新代码
+function countBodyChildren(){
+    var body_element = document.getElementsByTagName("body")[0];
+    alert(body_element.childNodes.length);
+    alert(body_element.nodeType);
+}
+
+function prepareGallery(){
+    if(!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function(){
+            // showPic(this);
+            // return false;
+            return !showPic(this);//如果图片占位符存在，就在图片占位符处切换图片，如果不存在图片占位符，就允许a标签的默认行为，跳转到相应的链接处
+        }
+        // links[i].onkeypress = links[i].onclick;
+    }
+}//图片库更新
+
+function addLoadEvent(func){
+    var oldonload = window.onload;
+    if(typeof window.onload != "function"){
+        window.onload = func;
+    }else{
+        window.onload = function(){
+            oldonload();
+            func();
+        }
+    }
+}//共享onload事件
+
+function insertAfter(newElement,targetElement){
+    var parent = targetElement.parentNode;
+    if(parent.lastChild == targetElement){
+        parent.appendChild(newElement);
+    }else{
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+
+}//在目标元素后面插入新元素
+
+function preparePlaceholder(){
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/Koala.jpg");//相对路径相对的是html文件的
+    placeholder.alt = "my image gallery";
+    var description = document.createElement("p");
+    description.id = "description";
+    var desctext = document.createTextNode("Choose an image");
+    description.appendChild(desctext);
+
+    // document.getElementsByTagName("body")[0].appendChild(placeholder);
+    // document.getElementsByTagName("body")[0].appendChild(description);//插入到body节点的最后，并不能插入到指定位置
+    // document.body.appendChild(placeholder);   //HTML-DOM
+    
+    var gallery = document.getElementById("imagegallery");
+    // gallery.parentNode.insertBefore(placeholder, gallery);
+    // gallery.parentNode.insertBefore(description, gallery);//将图片占位符插入图片库前面
+    insertAfter(placeholder,gallery);
+    insertAfter(description,placeholder);
+    
+}
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
